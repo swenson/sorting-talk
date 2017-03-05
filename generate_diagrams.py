2e5dc3830ac8aedd8167ab281a3397cd683f5c4e
Christopher Swenson <chris@caswenson.com>
2017-03-04 21:29:53 -0800

    First draft

--- generate_diagrams.py
+++ generate_diagrams.py
@@ -1,5 +1,8 @@
 import random
 
+# fname = "timsort-partially-sorted-%d.svg"
+fname = "timsort-random-%d.svg"
+
 random.seed(12345)
 
 unsorted_values = [random.randint(0, 63) for i in xrange(100)]
@@ -7,58 +10,71 @@ unsorted_values = [random.randint(0, 63) for i in xrange(100)]
 def sort(array, start, length):
   array[start:start + length] = sorted(array[start:start + length])
 
-sort(unsorted_values, 0, 13)
-sort(unsorted_values, 13, 16)
-sort(unsorted_values, 29, 30)
-unsorted_values[29:59] = list(reversed(unsorted_values[29:59]))
-sort(unsorted_values, 59, 41)
+# sort(unsorted_values, 0, 13)
+# sort(unsorted_values, 13, 16)
+# sort(unsorted_values, 29, 30)
+# unsorted_values[29:59] = list(reversed(unsorted_values[29:59]))
+# sort(unsorted_values, 59, 41)
 
 
 figures = []
 
-print """input boxes;
-prologues := 2;
-verbatimtex
-\documentclass{article}
-\pagestyle{empty}
-%\usepackage{mathpazo}
-\\begin{document}
-etex
+# print """input boxes;
+# prologues := 2;
+# verbatimtex
+# \documentclass{article}
+# \pagestyle{empty}
+# %\usepackage{mathpazo}
+# \\begin{document}
+# etex
+#
+# numeric u;
+# u := 2mm;
+#
+# vardef fillbox(expr ux, uy, width, height, shade) =
+#   fill (ux,uy)--(ux+width,uy)--(ux+width,uy-height)--(ux,uy-height)--cycle withpen pensquare scaled 1pt withcolor shade*white;
+# enddef;
+#
+# vardef drawbox(expr ux, uy, width, height, r, g, b) =
+#   draw (ux,uy)--(ux+width,uy)--(ux+width,uy-height)--(ux,uy-height)--cycle withpen pensquare scaled 3pt withcolor (r*red + g*green + b*blue);
+# enddef;
+# """
 
-numeric u;
-u := 2mm;
-
-vardef fillbox(expr ux, uy, width, height, shade) =
-  fill (ux,uy)--(ux+width,uy)--(ux+width,uy-height)--(ux,uy-height)--cycle withpen pensquare scaled 1pt withcolor shade*white;
-enddef;
-
-vardef drawbox(expr ux, uy, width, height, r, g, b) =
-  draw (ux,uy)--(ux+width,uy)--(ux+width,uy-height)--(ux,uy-height)--cycle withpen pensquare scaled 3pt withcolor (r*red + g*green + b*blue);
-enddef;
-"""
-
-figno = 1
+figno = 0
+height = 128
+stroke = 4
+width_mult = 4
 
 colors = []
-for i in xrange(100):
-  red = random.random()
-  green = random.random()
-  blue = random.random()
-  colors.append((red, green, blue))
+for i in xrange(3):
+  hue = i / 3.0 #random.random()
+  s = 1.0
+  l = 0.5
+  colors.append((hue, s, l))
+
+def hsl(h, s, l):
+    return "hsl(%.1f,%.1f%%,%.1f%%)" % (h*360.0, s*100.0, l*100.0)
+
+def rgb(r, g, b):
+    r = int(round(r * 255.0))
+    g = int(round(g * 255.0))
+    b = int(round(b * 255.0))
+    return "#%02x%02x%02x" % (r, g, b)
 
 def make_figure(values, stack=[]):
   global figno
-  text = "beginfig(%d);" % figno
-  text += "pickup pensquare scaled 1pt;"
+  text = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="%d" height="%d" viewBox="0 0 %d %d">' % (width_mult * 100, height, width_mult * 100, height)
   for i, v in enumerate(values):
-    text += "fillbox(%d * u, 0, u, 4*u, %f);" % (i, v / 63.0)
+      text += '  <rect style="fill:%s" x="%d" y="0" width="%d" height="%d" />\n' % (rgb(v/63.0,v/63.0,v/63.0), i*width_mult, width_mult, height)
 
   for (start, length), color in zip(stack, colors):
-    text += "drawbox(%d * u, 0, %d * u, 4 * u, %f, %f, %f);" % (start, length, color[0], color[1], color[2])
+    text += '  <rect style="fill:none;stroke:%s;stroke-width:%.1f" x="%d" y="0" width="%d" height="%d" />\n' % (hsl(*color), stroke, start*width_mult, length*width_mult, height)
 
-  text += "endfig; %% %d" % figno
+  # text += "endfig; %% %d" % figno
+  text += "</svg>"
   figno += 1
-  print text
+  with open(fname % figno, "w") as fout:
+      fout.write(text)
 
 def compute_minrun(size, max_size=16):
   max_bits = -2
@@ -78,8 +94,8 @@ def compute_minrun(size, max_size=16):
 
 minrun = compute_minrun(len(unsorted_values))
 stack = []
-print "%% minrun = %d"
-print "%% stack = %s" % str(stack)
+#print "%% minrun = %d"
+#print "%% stack = %s" % str(stack)
 
 make_figure(unsorted_values)
 
@@ -186,12 +202,12 @@ def merge_right(array, stack):
   stack.append((acurr, alen + blen))
 
 
-print "%% push_next"
+#print "%% push_next"
 push_next()
-print "%% stack = %s" % str(stack)
-print "%% push_next"
+#print "%% stack = %s" % str(stack)
+#print "%% push_next"
 push_next()
-print "%% push_next"
+#print "%% push_next"
 push_next()
 
 while stack[0][1] != len(array):
@@ -201,10 +217,10 @@ while stack[0][1] != len(array):
   push_next()
 
 
-footer = """
-verbatimtex
-\end{document}
-etex
-bye
-"""
-print footer
+# footer = """
+# verbatimtex
+# \end{document}
+# etex
+# bye
+# """
+# print footer
